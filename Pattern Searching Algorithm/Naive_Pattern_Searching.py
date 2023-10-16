5c925445a7c1a202be49a481b2c71ee5675b0fae
ConradKash <kakurucon1234@gmail.com>
2023-10-16 20:20:55 +0300

    naive_search_pattern algorithm

---
+++ Pattern Searching Algorithm/Naive_Pattern_Searching.py
@@ -0,0 +1,36 @@
+# Python3 program for Naive Pattern
+# Searching algorithm
+
+
+def search(pat, txt):
+	M = len(pat)
+	N = len(txt)
+
+	# A loop to slide pat[] one by one */
+	for i in range(N - M + 1):
+		j = 0
+
+		# For current index i, check
+		# for pattern match */
+		while(j < M):
+			if (txt[i + j] != pat[j]):
+				break
+			j += 1
+
+		if (j == M):
+			print("Pattern found at index ", i)
+
+
+# Driver's Code
+if __name__ == '__main__':
+	txt = "AABAACAADAABAAABAA"
+	pat = "AABA"
+	
+	# Function call
+	search(pat, txt)
+    #try it yourself
+	txt = input("Enter the text: ")
+	pat = input("Enter the pattern: ")
+	
+
+	search(pat, txt)
